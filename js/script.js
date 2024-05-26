f7897ae4c0a4d74bd595f034b98cdcc1e473fe8d
Ernesto Henrique <ernestohnm@gmail.com>
2024-05-26 02:07:39 -0300

    Atualização NavBar, Footer

--- js/script.js
+++ js/script.js
@@ -1,5 +1,31 @@
+//Carregar NavBar
+
+// Função para carregar o conteúdo do Navbar
+function carregarNavbar() {
+  fetch("navbar.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("navbar").innerHTML = data;
+    });
+}
+
+// Chamar a função para carregar o Navbar ao carregar a página
+window.addEventListener("DOMContentLoaded", carregarNavbar);
+
 //efeito do botão voltar ao Topo
 
 //Validação de Login
 
 //Ativar alert no botão cadastrar
+
+// Função para carregar o Footer
+function carregarFooter() {
+  fetch("footer.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("footer").innerHTML = data;
+    });
+}
+
+// Chamar a função para carregar o Navbar ao carregar a página
+window.addEventListener("DOMContentLoaded", carregarFooter);
